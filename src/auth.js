47453c59eae91118a8179b7f6634e1969b3131d2
Paul Scharnofske <asynts@gmail.com>
2022-10-04 22:30:59 +0200

    Implement OAuth properly
    -   We now use the "Authorization Code Flow" instead of the "Implicit
        Flow".  This is more secure and it's possible to refresh the access
        tokens.
    
    -   We now ask the user to provide us with the 'clientId' and
        'clientSecret'.  This makes little sense, but unfortunately Spotify
        won't have it any other way.
    
    -   All of this is currently untested and not hooked up to the frontend.

--- src/auth.js
+++ src/auth.js
@@ -1,23 +1,123 @@
 import * as oauth from "@panva/oauth4webapi";
 
-let SPOTIFY_CLIENT_ID = "e65e5a2379654a8782ac21646029ca66";
 let SPOTIFY_SCOPE = "";
-let SPOTIFY_REDIRECT_URL = "http://localhost:3000/auth_endpoint";
 
-export function getSpotifyToken() {
-    return localStorage.getItem("spotify-oauth-token");
+export function parseFromLocalStorage() {
+    let clientId =localStorage.getItem("spotify-oauth-client-id");
+    let clientSecret = localStorage.getItem("spotify-oauth-client-secret");
+    let clientBasicAuthorization = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
+
+    return {
+        clientId,
+        clientSecret,
+        clientBasicAuthorization,
+
+        savedState: localStorage.getItem("spotify-oauth-state"),
+        savedCodeVerifier: localStorage.getItem("spotify-oauth-code-verifier"),
+
+        accessToken: localStorage.getItem("spotify-oauth-access-token"),
+        refreshToken: localStorage.getItem("spotify-oauth-refresh-token"),
+    };
 }
 
-export function redirectToSpotifyAuthentication() {
+// This will redirect the user to Spotify.
+// The user is asked to confirm that they want to give us access.
+// Once confirmed they are redirected to the redirect URI which is handled by 'handleCodeResponseAsync'.
+export function redirectToSpotifyAuthentication({ clientId, clientSecret, redirectUri }) {
+    localStorage.setItem("spotify-oauth-client-id", clientId);
+    localStorage.setItem("spotify-oauth-client-secret", clientSecret);
+
     let state = oauth.generateRandomState();
     localStorage.setItem("spotify-oauth-state", state);
 
-    let authorizationUrl = new URL("https://accounts.spotify.com/authorize");
-    authorizationUrl.searchParams.set("client_id", SPOTIFY_CLIENT_ID);
-    authorizationUrl.searchParams.set("response_type", "token");
-    authorizationUrl.searchParams.set("redirect_uri", SPOTIFY_REDIRECT_URL);
+    // OAuth 2.0 Authorization Code Flow with PKCE.
+
+    let codeVerifier = oauth.generateRandomCodeVerifier();
+    localStorage.setItem("spotify-oauth-code-verifier", codeVerifier);
+    let codeChallenge = oauth.calculatePKCECodeChallenge(codeVerifier);
+
+    let authorizationUrl = new URL("https://accounts.spotify.com/api/token");
+    authorizationUrl.searchParams.set("client_id", clientId);
+    authorizationUrl.searchParams.set("response_type", "code");
+    authorizationUrl.searchParams.set("redirect_uri", redirectUri);
     authorizationUrl.searchParams.set("state", state);
     authorizationUrl.searchParams.set("scope", SPOTIFY_SCOPE);
+    authorizationUrl.searchParams.set("show_dialog", true);
+    authorizationUrl.searchParams.set("show_dialog", true);
+    authorizationUrl.searchParams.set("code_challenge_method", "S256");
+    authorizationUrl.searchParams.set("code_challenge", codeChallenge);
 
     window.location.href = authorizationUrl;
+}
+
+// We are provided with a code that we can exchange for an access token and a refresh token.
+export async function handleCodeResponseAsync({ code, state, redirectUri }) {
+    let {
+        savedState,
+        clientId,
+        savedCodeVerifier,
+        clientBasicAuthorization,
+    } = parseFromLocalStorage();
+
+    if (state !== savedState) {
+        return false;
+    }
+
+    let formData = new FormData();
+    formData.append("grant_type", "authorization_code");
+    formData.append("code", code);
+    formData.append("redirect_uri", redirectUri);
+    formData.append("client_id", clientId);
+    formData.append("code_verifier", savedCodeVerifier);
+
+    let response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        body: formData,
+        credentials: "include",
+        headers: new Headers({
+            "Authorization": `Basic ${clientBasicAuthorization}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+        }),
+    });
+
+    let json = await response.json();
+
+    if (response.ok) {
+        localStorage.setItem("spotify-oauth-access-token", json.access_token);
+        localStorage.setItem("spotify-oauth-refresh-token", json.refresh_token);
+
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Use refresh token to obtain up-to-date access token.
+export async function refreshAccessTokenAsync() {
+    let { clientBasicAuthorization, refreshToken, clientId } = parseFromLocalStorage();
+
+    let formData = new FormData();
+    formData.append("grant_type", "refresh_token");
+    formData.append("refresh_token", refreshToken);
+    formData.append("client_id", clientId);
+
+    let response = fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        body: formData,
+        credentials: "include",
+        headers: new Headers({
+            "Authorization": `Basic ${clientBasicAuthorization}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+        }),
+    });
+
+    let json = await response.json();
+
+    if (response.ok) {
+        localStorage.setItem("spotify-oauth-access-token", json.access_token);
+
+        return true;
+    } else {
+        return false;
+    }
 }
